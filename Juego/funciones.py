141f934776888dfe3330f099477a091cbba2af25
lautaro cardozo <lautycardozo01@gmail.com>
2025-06-13 09:30:18 -0300

    Agregue seleccionar_palabra

--- Juego/funciones.py
+++ Juego/funciones.py
@@ -7,6 +7,8 @@ def seleccionar_categoria(palabras):
 
     
     
-def seleccionar_palabra(palabras):
-    
+def seleccionar_palabra(llave,diccionario):
+    indice = random.randint(0,9)
+    palabra = diccionario[llave][indice]
+    return palabra
     

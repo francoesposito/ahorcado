f5d3f12442b2c9f9aec5a36e1c92e38e8a08b16b
Franco Espósito <francoespositomartin2006@gmail.com>
2025-06-13 09:31:43 -0300

    agrego seleccionar palabra

--- Juego/funciones.py
+++ Juego/funciones.py
@@ -2,11 +2,15 @@ from palabras import palabras
 import random
 
 
-def seleccionar_categoria(palabras):
-
+def seleccionar_categoria() -> str:
+    
+    llave = random.randint(1,4)
+    llave = str(llave)
+    
+    return llave
 
     
     
-def seleccionar_palabra(palabras):
+def seleccionar_palabra(llave):
     
     

cbd60d636251ea09f9a9604369a73af935e7f73c
Franco Espósito <francoespositomartin2006@gmail.com>
2025-06-13 09:26:55 -0300

    push

---
+++ Juego/funciones.py
@@ -0,0 +1,12 @@
+from palabras import palabras 
+import random
+
+
+def seleccionar_categoria(palabras):
+
+
+    
+    
+def seleccionar_palabra(palabras):
+    
+    
--- Juego/palabras.py
+++ Juego/palabras.py
@@ -1,6 +1,11 @@
 palabras = {
-      "programacion" : ["algoritmo", "compilador", "bucle", "variable", "funcion", "recursividad", "sintaxis", "objeto", "clase", "depurar"],
-      "videojuegos" : ["joystick", "consola", "pixel", "skin", "estrategia", "aventura", "boss", "multijugador", "checkpoint"],
-      "historia" : ["revolución","imperio","dictadura","cruzadas","colonización","renacimiento","feudalismo","independencia","esclavitud","constitución"],
-      "deportes" : ["futbol","maratón","olímpicos","penalti","basquet","esgrima","arquero","natacion","ciclismo","jinete"],
+      "1" : ["algoritmo", "compilador", "bucle", "variable", "funcion", "recursividad", "sintaxis", "objeto", "clase", "depurar"],
+      "2" : ["joystick", "consola", "pixel", "skin", "estrategia", "aventura", "boss", "multijugador", "checkpoint", "nivel"],
+      "3" : ["revolución","imperio","dictadura","cruzadas","colonización","renacimiento","feudalismo","independencia","esclavitud","constitución"],
+      "4" : ["futbol","maratón","olímpicos","penalti","basquet","esgrima","arquero","natacion","ciclismo","jinete"],
 }
+
+# "programacion" 1
+# "videojuegos" 2 
+# "historia" 3 
+# "deportes" 4

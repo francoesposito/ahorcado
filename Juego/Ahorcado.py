430a0e1948ac40adaf5047454bdb15041e0aca9e
Franco Espósito <francoespositomartin2006@gmail.com>
2025-06-13 10:44:24 -0300

    Agrego juego parcial

--- Juego/Ahorcado.py
+++ Juego/Ahorcado.py
@@ -3,7 +3,29 @@ from funciones import *
 import sys
 
 def jugar_ahorcado() -> None:
-    
+    seguir = "si"
+    primera = True
+    while seguir == "si":
+        categoria = seleccionar_categoria()
+        palabra = seleccionar_palabra(categoria, palabras)
+        palabra_oculta = mostrar_palabra_oculta(palabra)
+        termino = False
+        errores = 0
+        aciertos = 0
+        if primera == False:
+            seguir = input("¿Seguir jugando? si/no:  ")
+        while termino != True and seguir == "si":
+            print(palabra)
+            print(f"Categoría: {categoria}\nPalabra: {palabra_oculta}")
+            letra = input("Ingrese una letra: ") 
+            aciertos = sumar_aciertos(palabra, letra, aciertos, palabra_oculta)
+            palabra_oculta = actualizar_palabra_oculta(palabra, palabra_oculta, letra)
+            errores = sumar_errores(palabra, letra, errores)
+            print(f"Errores: {errores}, aciertos: {aciertos}")
+            termino = verificar_estado_juego(palabra, palabra_oculta, errores)
+            primera = False
+
+        
 
 
 
--- Juego/funciones.py
+++ Juego/funciones.py
@@ -20,19 +20,54 @@ def seleccionar_palabra(llave,diccionario):
 
 def actualizar_palabra_oculta(palabra:str, palabra_oculta:str, letra_ingresada:str) -> str:
     i = 0
+    palabra_oculta_lista = list(palabra_oculta)
     
     for letra in palabra:
         if letra_ingresada == palabra[i]:
-            palabra_oculta[i] = palabra_oculta
+            palabra_oculta_lista[i] = letra_ingresada
         i += 1
+    palabra_oculta = "".join(palabra_oculta_lista)
+    
         
     return palabra_oculta
+
+def buscar_letra(palabra, letra_ingresada):
+    encontrado = False
+    for i in range(len(palabra)):
+        if letra_ingresada == palabra[i]:
+            encontrado = True
+            break
+    return encontrado
+
+def sumar_errores(palabra, letra_ingresada, errores):
+    palabra = list(palabra)
+    i = 0 
+    for letra in palabra:
+        encontrado = buscar_letra(palabra, letra_ingresada)
+        if encontrado == False:
+            errores += 1
+            break
+    i += 1
+    return errores
+
+def sumar_aciertos(palabra, letra_ingresada, aciertos, palabra_oculta):
+    palabra = list(palabra)
+    palabra_oculta = palabra_oculta
+    i = 0 
+    for letra in palabra:
+        encontrado = buscar_letra(palabra, letra_ingresada)
+        encontrado_palabra_oculta = buscar_letra(palabra_oculta, letra_ingresada)
+        if encontrado == True and encontrado_palabra_oculta == False:
+            aciertos += 1
+            break
+    i += 1
+    return aciertos
 
 def verificar_estado_juego(palabra:str, palabra_oculta:str, errores:str) -> bool:
     
     termino = False
     
-    if (palabra == palabra_oculta) or errores == 7:
+    if (palabra == palabra_oculta) or errores > 7:
         termino = True
         
     return termino
@@ -43,9 +78,16 @@ def calcular_puntuacion_parcial(errores,aciertos):
 
 def calcular_puntuacion_final(puntaje_parcial,puntaje_final):
     puntaje_final += puntaje_parcial
+    
+def mostrar_palabra_oculta(palabra):
+    palabra_oculta = ""
+    for letra in palabra:
+        palabra_oculta += "_"
+    return palabra_oculta
 
-llave = seleccionar_categoria()
 
-palabra = seleccionar_palabra(llave, palabras)
+# llave = seleccionar_categoria()
 
-print(f"Categoría: {categorias[llave]} \npalabra elegida: {palabra}")
+# palabra = seleccionar_palabra(llave, palabras)
+
+# print(f"Categoría: {categorias[llave]} \npalabra elegida: {palabra}")

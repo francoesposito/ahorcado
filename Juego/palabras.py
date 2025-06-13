33e2f2dae08d73edf0ffb3371ce6e21569212049
lautaro cardozo <lautycardozo01@gmail.com>
2025-06-13 09:18:07 -0300

    agregue palabras

--- Juego/palabras.py
+++ Juego/palabras.py
@@ -1,6 +1,6 @@
 palabras = {
       "programacion" : ["algoritmo", "compilador", "bucle", "variable", "funcion", "recursividad", "sintaxis", "objeto", "clase", "depurar"],
-      "historia" : [],
-      "deportes" : [],
+      "historia" : ["revolución","imperio","dictadura","cruzadas","colonización","renacimiento","feudalismo","independencia","esclavitud","constitución"],
+      "deportes" : ["futbol","maratón","olímpicos","penalti","basquet","esgrima","arquero","natacion","ciclismo","jinete"],
       "videojuegos" : ["joystick", "consola", "pixel"]
         }
